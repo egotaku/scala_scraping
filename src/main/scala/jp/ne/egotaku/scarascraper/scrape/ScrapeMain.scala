9b527cfd7e4d85ec91f2440862e2acab65a49e54
takuya_st <takuya_st@nyle.co.jp>
2016-11-22 19:00:12 +0900

    kamon

--- src/main/scala/jp/ne/egotaku/scarascraper/scrape/ScrapeMain.scala
+++ src/main/scala/jp/ne/egotaku/scarascraper/scrape/ScrapeMain.scala
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter
 
 object ScrapeMain {
   def main(args: Array[String]): Unit = {
+    kamon.Kamon.start()
     //val scraper = new ScrapeImpl()
     //val targetUrl = "https://www.google.co.jp/search?q=%E3%81%B5%E3%81%A8%E3%82%82%E3%82%82&safe=off&espv=2&biw=2374&bih=1263&source=lnms&tbm=isch&sa=X&ved=0ahUKEwji9YrPtuTPAhXhilQKHVvrBHkQ_AUIBigB"
     //val targetUrl = "http://seiga.nicovideo.jp/illust/ranking/point/hourly/g_fanart"
